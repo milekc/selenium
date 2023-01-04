935e8020d42fe55cee06b9ce3b4139b15d5ef3c0
Milekc <milekc@yandex.ru>
2023-01-04 15:16:12 +0300

    "2.2 step 3 finaly"

--- selenium_course/test.py
+++ selenium_course/test.py
@@ -1,35 +1,28 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
-import math
+from selenium.webdriver.support.ui import Select
 
-def calc(x):
-  return str(math.log(abs(12*math.sin(int(x)))))
+
+def calc(x, y):
+    return int(x) + int(y)
 
 
 try:
-    link = "http://suninjuly.github.io/get_attribute.html"
+    link = "http://suninjuly.github.io/selects1.html"
     browser = webdriver.Chrome()
     browser.get(link)
 
-    # Переменные для поиска
-    value1, value2, value3 = '.first[placeholder*="name"]', '.second[placeholder*="name"]', '.third[placeholder*="email"]'
-
     # Обработка формулы
-    x_element = browser.find_element(By.ID, 'treasure')
-    x = x_element.get_attribute('valuex')
-    y = calc(x)
-
-
+    x_element = browser.find_element(By.ID, "num1")
+    y_element = browser.find_element(By.ID, "num2")
+    x = x_element.text
+    y = y_element.text
 
     # Ваш код, который заполняет обязательные поля
-    input1 = browser.find_element(By.CSS_SELECTOR, '#answer')
-    input1.send_keys(y)
-    input2 = browser.find_element(By.CSS_SELECTOR, '#robotCheckbox')
-    input2.click()
-    input3 = browser.find_element(By.CSS_SELECTOR, '#robotsRule')
-    input3.click()
-
+    select = Select(browser.find_element(By.TAG_NAME, "select"))
+    select.select_by_value(str(calc(x, y)))
 
     # Отправляем заполненную форму
     button = browser.find_element(By.CSS_SELECTOR, "button.btn")
