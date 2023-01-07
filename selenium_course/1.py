026f8bd6bac4f44247fa780a2f8712c8653b6cc2
Milekc <milekc@yandex.ru>
2023-01-07 20:38:45 +0300

    2.4 step 8

--- selenium_course/1.py
+++ selenium_course/1.py
@@ -1,5 +1,6 @@
-k  = 3
-r = k + 2
-f = r + 7
-x, y, z = map(int, input().split())
-print(k * x + r  * y + f * z)
+s = '45 45 66 21 94 62 50 13 96'
+l = list(map(int, input().split()))
+d = dict([(l[-2], l[-1])])
+for i in l[-3::-1]:
+    d = dict([(i, d)])
+print(d)
